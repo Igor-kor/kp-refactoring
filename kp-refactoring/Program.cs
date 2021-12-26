3bea1342befb4bb0fba29f48e9c8d203e3439587
Igor Sharangiya <a.sharangia@harddays.dev>
2021-12-26 17:09:39 +0300

    init commit

---
+++ kp-refactoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kp_refactoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
